894cbbff153775e1d3831404faf66590cd347d07
dekatora <pilust131@naver.com>
2025-12-03 17:07:12 +0900

    password session에 넣는 부분 제거 #2

--- bbs/ajax.mb_password.php
+++ bbs/ajax.mb_password.php
@@ -5,10 +5,6 @@ include_once(G5_LIB_PATH . '/register_sub.lib.php');
 $mb_password = isset($_POST['reg_mb_password']) ? trim($_POST['reg_mb_password']) : '';
 $mb_password_re = isset($_POST['reg_mb_password_re']) ? trim($_POST['reg_mb_password_re']) : '';
 
-set_session('ss_check_mb_password', '');
-
 if ($msg = empty_mb_password($mb_password))     die($msg);
 if ($msg = valid_mb_password($mb_password))     die($msg);
 if ($msg = count_mb_password($mb_password))     die($msg);
-
-set_session('ss_check_mb_password', $mb_password);
