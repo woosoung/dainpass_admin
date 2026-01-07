5e29f07dddbf1c86ba3ff0c2866b8c7bb1184d7a
dekatora <pilust131@naver.com>
2026-01-07 13:32:30 +0900

    shop_id 노출 제거 #41

--- adm/_z01/staff_list_update.php
+++ adm/_z01/staff_list_update.php
@@ -9,12 +9,6 @@ $result = check_shop_access();
 $shop_id = $result['shop_id'];
 
 $w = isset($_POST['w']) ? trim($_POST['w']) : '';
-$post_shop_id = isset($_POST['shop_id']) ? (int)$_POST['shop_id'] : 0;
-
-// 가맹점측 관리자는 자신의 가맹점만 수정 가능
-if ($post_shop_id != $shop_id) {
-    alert('접속할 수 없는 페이지 입니다.');
-}
 
 $sst = isset($_POST['sst']) ? trim($_POST['sst']) : '';
 $sod = isset($_POST['sod']) ? trim($_POST['sod']) : '';
